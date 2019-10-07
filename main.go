3d87aa2a976aaed100df367aa089160719afe877
sky0621 <sky14work@gmail.com>
2019-10-08 08:59:11 +0900

    continue

--- main.go
+++ main.go
@@ -2,28 +2,60 @@ package main
 
 import (
 	"fmt"
-	"github.com/tealeg/xlsx"
 	"os"
+
+	"github.com/tealeg/xlsx"
 )
 
+// TODO: 機能実現優先の雑実装
 func main() {
+	/*
+	 * Input -> Structured
+	 */
 	excelFileName := "テーブル定義書「商品」.xlsx"
 	xlFile, err := xlsx.OpenFile(excelFileName)
 	if err != nil {
 		os.Exit(-1)
 	}
+
+	var worksheets []*WorkSheet
+
 	for sn, sheet := range xlFile.Sheets {
-		fmt.Printf("\n<SheetNo: %d>\n", sn)
+		ws := &WorkSheet{No: sn, Name: sheet.Name}
 		for rn, row := range sheet.Rows {
-			fmt.Printf("\n<RowNo: %d>\n", rn)
+			r := &Row{No: rn}
 			for cn, cell := range row.Cells {
-				text := cell.String()
-				//nf := cell.GetNumberFormat()
-				//fm := cell.Formula()
-				//st := cell.GetStyle()
-				//fmt.Printf("[Value: %s][NumberFormat: %s][Formula: %s][Style: %#v]\n\n", text, nf, fm, st)
-				fmt.Printf("[%d] %s\n", cn, text)
+				c := &Cell{No: cn, Val: cell.String()}
+				r.Cells = append(r.Cells, c)
 			}
+			ws.Rows = append(ws.Rows, r)
 		}
+		worksheets = append(worksheets, ws)
 	}
+
+	fmt.Println(worksheets)
+
+	/*
+	 * To Migration File
+	 */
+
+	/*
+	 * To Go Struct
+	 */
+}
+
+type WorkSheet struct {
+	No   int
+	Name string
+	Rows []*Row
+}
+
+type Row struct {
+	No    int
+	Cells []*Cell
+}
+
+type Cell struct {
+	No  int
+	Val string
 }
